a2d5cf59e5637c53582fdbbfa3500be59195a8eb
vanuan <vanuan@gmail.com>
2016-10-24 00:03:40 +0300

    Bank open monetary data

---
+++ bank.js
@@ -0,0 +1,86 @@
+var data = [{"dt":"06.2016","txt":"Грошова маса","id_api":"M3","level":1,"parent":"Monetary","freq":"M","k076txt":"Усьогo","value":1036028,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Готівкові кошти в обігу поза депозитними корпораціями","id_api":"М0","level":4,"parent":"M1","freq":"M","k076txt":"Усьогo","value":287103,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"M2","id_api":"M2","level":2,"parent":"M3","freq":"M","k076txt":"Усьогo","value":1035785,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Інші депозити, що включаються в грошову масу (входять до M2-M1)","id_api":"OtherDep","level":3,"parent":"M3","freq":"M","k076txt":"Усьогo","value":423219,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Переказні депозити в іноземній валюті, що включаються в грошову масу (входять до M2-M1)","id_api":"TransDepFC","level":3,"parent":"M2","freq":"M","k076txt":"Усьогo","value":127289,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Переказні депозити в національній валюті, що включаються в грошову масу (M1-M0)","id_api":"TransDepNC","level":4,"parent":"M1","freq":"M","k076txt":"Усьогo","value":198174,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"M1","id_api":"M1","level":3,"parent":"M2","freq":"M","k076txt":"Усьогo","value":485277,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Інші депозити в національній валюті, що включаються в грошову масу","id_api":"OtherDepNC","level":4,"parent":"OtherDep","freq":"M","k076txt":"Усьогo","value":205159,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Інші депозити в іноземній валюті, що включаються в грошову масу","id_api":"OtherDepFC","level":4,"parent":"OtherDep","freq":"M","k076txt":"Усьогo","value":218060,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Цінні папери, крім акцій, що включаються в грошову масу, в національній валюті","id_api":"SecOthSharNC","level":3,"parent":"SecOthShar","freq":"M","k076txt":"Усьогo","value":55,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Цінні папери, крім акцій, що включаються в грошову масу (М3-М2)","id_api":"SecOthShar","level":2,"parent":"M3","freq":"M","k076txt":"Усьогo","value":243,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Цінні папери, крім акцій, що включаються в грошову масу, в іноземній валюті","id_api":"SecOthSharFC","level":3,"parent":"SecOthShar","freq":"M","k076txt":"Усьогo","value":188,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Інші депозити, що включаються в грошову масу (входять до M2-M1)","id_api":"OtherDep","level":3,"parent":"M3","freq":"M","k076txt":"Інші сектори економіки","value":307041,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Переказні депозити в іноземній валюті, що включаються в грошову масу (входять до M2-M1)","id_api":"TransDepFC","level":3,"parent":"M2","freq":"M","k076txt":"Інші сектори економіки","value":38871,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Переказні депозити в національній валюті, що включаються в грошову масу (M1-M0)","id_api":"TransDepNC","level":4,"parent":"M1","freq":"M","k076txt":"Інші сектори економіки","value":82681,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Інші депозити в національній валюті, що включаються в грошову масу","id_api":"OtherDepNC","level":4,"parent":"OtherDep","freq":"M","k076txt":"Інші сектори економіки","value":125560,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Інші депозити в іноземній валюті, що включаються в грошову масу","id_api":"OtherDepFC","level":4,"parent":"OtherDep","freq":"M","k076txt":"Інші сектори економіки","value":181480,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Цінні папери, крім акцій, що включаються в грошову масу, в національній валюті","id_api":"SecOthSharNC","level":3,"parent":"SecOthShar","freq":"M","k076txt":"Інші сектори економіки","value":1,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Цінні папери, крім акцій, що включаються в грошову масу (М3-М2)","id_api":"SecOthShar","level":2,"parent":"M3","freq":"M","k076txt":"Інші сектори економіки","value":188,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Цінні папери, крім акцій, що включаються в грошову масу, в іноземній валюті","id_api":"SecOthSharFC","level":3,"parent":"SecOthShar","freq":"M","k076txt":"Інші сектори економіки","value":188,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Інші депозити, що включаються в грошову масу (входять до M2-M1)","id_api":"OtherDep","level":3,"parent":"M3","freq":"M","k076txt":"Інші нефiнансовi корпорацiї","value":76440,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Переказні депозити в іноземній валюті, що включаються в грошову масу (входять до M2-M1)","id_api":"TransDepFC","level":3,"parent":"M2","freq":"M","k076txt":"Інші нефiнансовi корпорацiї","value":31567,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Переказні депозити в національній валюті, що включаються в грошову масу (M1-M0)","id_api":"TransDepNC","level":4,"parent":"M1","freq":"M","k076txt":"Інші нефiнансовi корпорацiї","value":94440,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Інші депозити в національній валюті, що включаються в грошову масу","id_api":"OtherDepNC","level":4,"parent":"OtherDep","freq":"M","k076txt":"Інші нефiнансовi корпорацiї","value":53446,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Інші депозити в іноземній валюті, що включаються в грошову масу","id_api":"OtherDepFC","level":4,"parent":"OtherDep","freq":"M","k076txt":"Інші нефiнансовi корпорацiї","value":22994,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Цінні папери, крім акцій, що включаються в грошову масу, в національній валюті","id_api":"SecOthSharNC","level":3,"parent":"SecOthShar","freq":"M","k076txt":"Інші нефiнансовi корпорацiї","value":25,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Цінні папери, крім акцій, що включаються в грошову масу (М3-М2)","id_api":"SecOthShar","level":2,"parent":"M3","freq":"M","k076txt":"Інші нефiнансовi корпорацiї","value":25,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Цінні папери, крім акцій, що включаються в грошову масу, в іноземній валюті","id_api":"SecOthSharFC","level":3,"parent":"SecOthShar","freq":"M","k076txt":"Інші нефiнансовi корпорацiї","value":0,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Інші депозити, що включаються в грошову масу (входять до M2-M1)","id_api":"OtherDep","level":3,"parent":"M3","freq":"M","k076txt":"Державнi нефiнансовi корпорацiї","value":22930,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Переказні депозити в іноземній валюті, що включаються в грошову масу (входять до M2-M1)","id_api":"TransDepFC","level":3,"parent":"M2","freq":"M","k076txt":"Державнi нефiнансовi корпорацiї","value":54242,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Переказні депозити в національній валюті, що включаються в грошову масу (M1-M0)","id_api":"TransDepNC","level":4,"parent":"M1","freq":"M","k076txt":"Державнi нефiнансовi корпорацiї","value":15021,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Інші депозити в національній валюті, що включаються в грошову масу","id_api":"OtherDepNC","level":4,"parent":"OtherDep","freq":"M","k076txt":"Державнi нефiнансовi корпорацiї","value":15143,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Інші депозити в іноземній валюті, що включаються в грошову масу","id_api":"OtherDepFC","level":4,"parent":"OtherDep","freq":"M","k076txt":"Державнi нефiнансовi корпорацiї","value":7787,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Цінні папери, крім акцій, що включаються в грошову масу, в національній валюті","id_api":"SecOthSharNC","level":3,"parent":"SecOthShar","freq":"M","k076txt":"Державнi нефiнансовi корпорацiї","value":0,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Цінні папери, крім акцій, що включаються в грошову масу (М3-М2)","id_api":"SecOthShar","level":2,"parent":"M3","freq":"M","k076txt":"Державнi нефiнансовi корпорацiї","value":0,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Цінні папери, крім акцій, що включаються в грошову масу, в іноземній валюті","id_api":"SecOthSharFC","level":3,"parent":"SecOthShar","freq":"M","k076txt":"Державнi нефiнансовi корпорацiї","value":0,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Інші депозити, що включаються в грошову масу (входять до M2-M1)","id_api":"OtherDep","level":3,"parent":"M3","freq":"M","k076txt":"Регiональнi та мiсцевi органи державного управлiння","value":0,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Переказні депозити в іноземній валюті, що включаються в грошову масу (входять до M2-M1)","id_api":"TransDepFC","level":3,"parent":"M2","freq":"M","k076txt":"Регiональнi та мiсцевi органи державного управлiння","value":0,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Переказні депозити в національній валюті, що включаються в грошову масу (M1-M0)","id_api":"TransDepNC","level":4,"parent":"M1","freq":"M","k076txt":"Регiональнi та мiсцевi органи державного управлiння","value":0,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Інші депозити в національній валюті, що включаються в грошову масу","id_api":"OtherDepNC","level":4,"parent":"OtherDep","freq":"M","k076txt":"Регiональнi та мiсцевi органи державного управлiння","value":0,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Інші депозити в іноземній валюті, що включаються в грошову масу","id_api":"OtherDepFC","level":4,"parent":"OtherDep","freq":"M","k076txt":"Регiональнi та мiсцевi органи державного управлiння","value":0,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Цінні папери, крім акцій, що включаються в грошову масу, в національній валюті","id_api":"SecOthSharNC","level":3,"parent":"SecOthShar","freq":"M","k076txt":"Регiональнi та мiсцевi органи державного управлiння","value":0,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Цінні папери, крім акцій, що включаються в грошову масу (М3-М2)","id_api":"SecOthShar","level":2,"parent":"M3","freq":"M","k076txt":"Регiональнi та мiсцевi органи державного управлiння","value":0,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Цінні папери, крім акцій, що включаються в грошову масу, в іноземній валюті","id_api":"SecOthSharFC","level":3,"parent":"SecOthShar","freq":"M","k076txt":"Регiональнi та мiсцевi органи державного управлiння","value":0,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Інші депозити, що включаються в грошову масу (входять до M2-M1)","id_api":"OtherDep","level":3,"parent":"M3","freq":"M","k076txt":"Iншi фiнансовi корпорацiї","value":16808,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Переказні депозити в іноземній валюті, що включаються в грошову масу (входять до M2-M1)","id_api":"TransDepFC","level":3,"parent":"M2","freq":"M","k076txt":"Iншi фiнансовi корпорацiї","value":2608,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Переказні депозити в національній валюті, що включаються в грошову масу (M1-M0)","id_api":"TransDepNC","level":4,"parent":"M1","freq":"M","k076txt":"Iншi фiнансовi корпорацiї","value":6032,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Інші депозити в національній валюті, що включаються в грошову масу","id_api":"OtherDepNC","level":4,"parent":"OtherDep","freq":"M","k076txt":"Iншi фiнансовi корпорацiї","value":11010,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Інші депозити в іноземній валюті, що включаються в грошову масу","id_api":"OtherDepFC","level":4,"parent":"OtherDep","freq":"M","k076txt":"Iншi фiнансовi корпорацiї","value":5798,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Цінні папери, крім акцій, що включаються в грошову масу, в національній валюті","id_api":"SecOthSharNC","level":3,"parent":"SecOthShar","freq":"M","k076txt":"Iншi фiнансовi корпорацiї","value":30,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Цінні папери, крім акцій, що включаються в грошову масу (М3-М2)","id_api":"SecOthShar","level":2,"parent":"M3","freq":"M","k076txt":"Iншi фiнансовi корпорацiї","value":30,"tzepname":"залишки коштів на кінець періоду, млн. грн."},{"dt":"06.2016","txt":"Цінні папери, крім акцій, що включаються в грошову масу, в іноземній валюті","id_api":"SecOthSharFC","level":3,"parent":"SecOthShar","freq":"M","k076txt":"Iншi фiнансовi корпорацiї","value":0,"tzepname":"залишки коштів на кінець періоду, млн. грн."}];
+
+data = data.map(function(d) {
+  d.k076txt = d.k076txt.replace(/i/g, "i");
+  d.k076txt = d.k076txt.replace(/o/g, "о");
+  return d;
+});
+
+var seen = [];
+var flat = [];
+data.forEach(function (element) {
+  if (seen.indexOf(element.id_api) === -1) {
+    flat.push({
+      "text": element.txt,
+      "id": element.id_api,
+      "parentId": element.parent
+    });
+    seen.push(element.id_api)
+  }
+});
+flat.push({ "text": "Грошові агрегати", "id": "Monetary" })
+var children = getLeafs(data);
+children.forEach(function(child) {
+  Object.keys(child.components).forEach(function(key) {
+    flat.push({
+      "text": key,
+      "id": child.id_api+key,
+      "parentId": child.id_api,
+      "value": child.components[key]
+    });
+  })
+});
+window.flat = flat;
+//console.log(JSON.stringify(flat, ' ', 2))
+
+function getLeafs(data) {
+  var elements = data;
+  var dict = {};
+  function getId(element) {
+    return element.level + element.id_api ;
+  }
+  function getParentId(element) {
+    return (element.level - 1) + element.parent ;
+  }
+  function getNode(id) {
+    var el = dict[id];
+    if(!el) {
+      el = dict[id] = {};
+    }
+    return el;
+  }
+
+  elements.forEach(function(element) {
+    var id = getId(element)
+    var node = getNode(id)
+    //delete element.k076txt;
+    delete element.dt;
+    ["freq", "txt", "level", "tzepname", "id_api", "parent"].forEach(function(key) {
+      node[key] = element[key]
+    });
+
+    if (!node.components) {
+      node.components = {};
+    }
+    node.components[element.k076txt] = element.value;
+    var parent = getNode(getParentId(element))
+    var children = parent.children;
+    if (!children) {
+      children = parent.children = [];
+    }
+    children.push(node);
+  });
+  var leafKeys = Object.keys(dict).filter(function(key) {
+    return !dict[key].hasOwnProperty("children");
+  });
+  var objects = leafKeys.map(function(key) {
+    if(Object.keys(dict[key].components).length > 1) {
+      delete dict[key].components["Усього"];
+    } else {
+      dict[key].components["Готівка (M0)"] = dict[key].components["Усього"];
+      delete dict[key].components["Усього"];
+    }
+    return dict[key];
+  });
+  return objects;
+}
---
+++ treemap.js
@@ -0,0 +1,144 @@
+var root = d3.stratify()
+    .id(function(d) { return d.id; })
+    .parentId(function(d) { return d.parentId; })
+    (window.flat);
+root.sum(function(d) { return d.value; });
+    //.sort(function(a, b) { return b.value - a.value; });
+/*
+var width=100, height=100;
+var treemap = d3.treemap()
+    .size([100, 100])
+    .padding(1)
+    .round(true);
+treemap(root);
+
+var div = d3.select("#d3-treemap").append("div");
+console.log(div)
+  //  .style("position", "relative")
+  //  .style("width", (width + margin.left + margin.right) + "px")
+  //  .style("height", (height + margin.top + margin.bottom) + "px")
+  //  .style("left", margin.left + "px")
+  //  .style("top", margin.top + "px");
+  var node = div.datum(root).data(treemap.nodes)//.enter().append("div");
+  //    .attr("class", "node");
+
+  svg.selectAll("path")
+      .data(partition(root).descendants())
+    .enter().append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color((d.children ? d : d.parent).data.name); })
+      .on("click", click)
+    .append("title")
+      .text(function(d) { return d.data.name + "\n" + formatNumber(d.value); });
+
+
+//  node
+//      .data(treemap.value(value).nodes)
+
+
+
+console.log(root)
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+var svg = d3.select("svg"),
+    diameter = +svg.attr("width"),
+    g = svg.append("g").attr("transform", "translate(2,2)"),
+    format = d3.format(",d");
+
+    var pack = d3.pack()
+        .size([diameter - 4, diameter - 4]);
+
+
+  var node = g.selectAll(".node")
+    .data(pack(root).descendants())
+    .enter().append("g")
+      .attr("class", function(d) { return d.children ? "node" : "leaf node"; })
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+  node.append("title")
+      .text(function(d) { return d.data.text + "\n" + format(d.value / 1000) + " млрд грн"; });
+
+  node.append("circle")
+      .attr("r", function(d) { return d.r; });
+
+  node.filter(function(d) { return !d.children; }).append("text")
+      .attr("dy", "0.3em")
+      .text(function(d) { return d.data.text.substring(0, d.r / 3); });
+
+
+
+/*
+var width = 500,
+    height = 500,
+    radius = (Math.min(width, height) / 2) - 10;
+
+var formatNumber = d3.format(",d");
+
+var x = d3.scaleLinear()
+    .range([0, 2 * Math.PI]);
+
+var y = d3.scaleSqrt()
+    .range([0, radius]);
+
+var color = d3.scaleOrdinal(d3.schemeCategory20c);
+//var color = d3.scale.category20c();
+
+var partition = d3.partition();
+
+var arc = d3.arc()
+    .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x0))); })
+    .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x1))); })
+    .innerRadius(function(d) { return Math.max(0, y(d.y0)); })
+    .outerRadius(function(d) { return Math.max(0, y(d.y1)); });
+
+
+var svg = d3.select("#d3-treemap").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
+
+
+  svg.selectAll("path")
+      .data(partition(root).descendants())
+    .enter().append("path")
+      .attr("d", arc)
+      .style("stroke", "#fff")
+      .style("fill", function(d) { return color(d.children ? d.depth + d.data.text : d.data.text); })
+      .style("fill-rule", "evenodd")
+      .on("click", click)
+    .append("title")
+      .text(function(d) { return d.data.text + "\n" + formatNumber(d.value); });
+
+  svg.selectAll("path").append("text")
+      .attr("dy", "0.3em")
+      .text(function(d) { return d.data.text.substring(0, d.r / 3); });
+
+function click(d) {
+  svg.transition()
+      .duration(750)
+      .tween("scale", function() {
+        var xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
+            yd = d3.interpolate(y.domain(), [d.y0, 1]),
+            yr = d3.interpolate(y.range(), [d.y0 ? 20 : 0, radius]);
+        return function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
+      })
+    .selectAll("path")
+      .attrTween("d", function(d) { return function() { return arc(d); }; });
+}
+
+d3.select(self.frameElement).style("height", height + "px");
+*/
